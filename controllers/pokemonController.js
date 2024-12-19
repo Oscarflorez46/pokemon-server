15e2b2d3bfb3e8f428e71b0308a88a38dfe91249
Oscarflorez46 <oscarflorez64@gmail.com>
2024-12-18 21:19:28 -0500

    capturar pokemon

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,3 +1,4 @@
+const { json } = require("express")
 const StatusPokemon = require ("../models/pokemonModels")
 exports.test =(req,res)=>{
     console.log("hola controller")
@@ -22,6 +23,50 @@ exports.getPokemonStatus = async (req,res)=>{
         const status = await StatusPokemon.find({})
         res.status(200).json(status)
     }catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
+    }
+}
+exports.getPokemonByPokemonId = async (req,res)=>{
+    try {
+        const pokemon_id =req.params.pokemon_id;
+        let pokemon = await StatusPokemon.findOne({pokemon_id:pokemon_id});
+        if(!pokemon){
+            res.status(404).json({message:"Pokemon not found"})
+        }else{
+            res.status(200).json(pokemon)
+        }
+        
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
+    }
+}
+exports.catchPokemonByPokemonId = async (req,res)=>{
+    try {
+        const pokemon_id =req.params.pokemon_id;
+        const pokemonStatusId = req.body.pokemon_id
+        if(pokemon){
+
+        }
+        let pokemon = await StatusPokemon.findOne({pokemon_id:pokemon_id});
+        if(!pokemon){
+            return res.status(400),json({message:"Bad Request, pokemon not view"})
+        } 
+        else if (pokemon.catch){
+            return res.status(200).json(pokemon)
+        }else{
+            pokemon = await StatusPokemon
+            .findOneAndReplace({pokemon_id:pokemon_id},{
+                pokemon_id: pokemon_id,
+                view: true,
+                catch: true,
+                in_team : false
+            },{new:true}
+        )
+            return res.status(200).json(pokemon)
+        }
+    } catch (error) {
         console.error(error)
         return res.status(500).json({error})
     }
